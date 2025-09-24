4c2b6b34f4d5a7aa8f64909cfa2e96ac4b61ef40
Andre07G <eleal087@gmail.com>
2025-09-23 21:35:22 -0500

    feat: :sparkles: Add Gestion_financiera entity model class by@Andre07g

---
+++ models/Gestion_financiera.js
@@ -0,0 +1,17 @@
+export default class MovimientoFinanciero {
+    constructor({ tipo, cliente, plan, concepto, pago, fecha = new Date(), descripcion}) {
+        this.tipo = tipo; 
+        this.pago = pago;
+        this.fecha = fecha;
+        if (tipo === "Ingreso") {
+            this.clienteID = cliente._id;
+            this.clienteNombre = cliente.nombre;
+            this.planID = plan._id;
+            this.planNombre = plan.nombre;
+        }
+        if (tipo === "Egreso") {
+            this.concepto = concepto;
+            this.descripcion = descripcion;
+        }
+    }
+}
