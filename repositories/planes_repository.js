a741cbfcbaefc54e326c6810eb1887890ef4d1ac
Andre07G <eleal087@gmail.com>
2025-09-24 16:02:03 -0500

    feat: :sparkles: Add Planes repository Created PlanesRepository.js to handle CRUD operations with MongoDB for the Planes collection. by@Andre07g

---
+++ repositories/planes_repository.js
@@ -0,0 +1,25 @@
+import {ObjectId} from "mongodb"
+
+export default class PlanesRepositorio{
+    constructor(base){
+        this.coleccion=base.collection("planes")
+    }
+
+    async crear(plan){
+        return await this.coleccion.insertOne(plan)
+    }
+
+    async listar(){
+        return await this.coleccion.find().toArray();
+    }
+
+    async editar(id,data){
+        return await this.coleccion.updateOne({_id:new ObjectId(id)},{$set:data});
+    }
+
+    async eliminar(plan){
+        return await this.coleccion.deleteOne({_id:new ObjectId(plan._id)});
+    }
+
+}
+
