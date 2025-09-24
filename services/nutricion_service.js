d39078557333d8829cb5afbb7f1ec57e2d714efe
Andre07G <eleal087@gmail.com>
2025-09-24 18:43:53 -0500

    feat: :sparkles: Add Nutricion service Created nutricion_service.js using NutricionRepository, enabling nutrition records assignment to clients. by@Andre07g

---
+++ services/nutricion_service.js
@@ -0,0 +1,30 @@
+import NutricionRepositorio from "../repositories/nutricion.repository.js";
+
+export default class NutricionService{
+    constructor(base,cliente){
+        this.repositorio=new NutricionRepositorio(base, cliente);
+    }
+
+    async crearPlanNutricional(planNutricionalObj){
+        if(!planNutricionalObj.nombre ||!planNutricionalObj.almuerzo ||!planNutricionalObj.desayuno ||!planNutricionalObj.cena || !planNutricionalObj.planId){
+            throw new Error("El plan alimenticio debe tener nombre, almuerzo, desayuno y cena");
+        }
+        return await this.repositorio.crear(planNutricionalObj)
+    }
+
+    async editarPlanNutricional(id,planNutricionalEditado){
+       if(!planNutricionalObj.nombre ||!planNutricionalObj.almuerzo ||!planNutricionalObj.desayuno ||!planNutricionalObj.cena || !planNutricionalObj.planId){
+            throw new Error("El plan alimenticio debe tener nombre, almuerzo, desayuno y cena");
+        }
+        return await this.repositorio.editar(id, planNutricionalEditado)
+    }
+
+    async listarPlanesNutricionales(){
+        return await this.repositorio.listar();
+    }
+
+    async eliminarPlanNutricional(planNutricionalObj){
+        return await this.repositorio.eliminar(planNutricionalObj)
+    }
+
+}
