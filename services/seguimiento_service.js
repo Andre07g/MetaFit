c4a2ba19f50625614caa7af32be14e7fef52fad7
Andre07G <eleal087@gmail.com>
2025-09-24 18:44:05 -0500

    feat: :sparkles: Add Seguimiento service Created seguimiento_service.js using SeguimientoRepository with business logic for CRUD operations. by@Andre07g

---
+++ services/seguimiento_service.js
@@ -0,0 +1,23 @@
+import SeguimientoRepositorio from "../repositories/seguimiento_repository.js";
+
+export default class SeguimientoService{
+    constructor(base,cliente){
+        this.repositorio=new SeguimientoRepositorio(base, cliente);
+    }
+
+    async crearSeguimiento(registroObj){
+        if(!registroObj.clienteId ||!registroObj.pesoActual ||!registroObj.porcentajeDeGrasa ||!registroObj.medidas || !registroObj.comentarios || !registroObj.fecha){
+            throw new Error("Datos incompletos o invalidos para el registro");
+        }
+        return await this.repositorio.crear(registroObj)
+    }
+
+    async listarSeguimientos(){
+        return await this.repositorio.listar();
+    }
+
+    async eliminarSeguimiento(registroObj){
+        return await this.repositorio.eliminar(registroObj)
+    }
+
+}
