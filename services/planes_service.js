ff55c05a56fa82ae1eb02c1170cc7f3c4e591d42
Andre07G <eleal087@gmail.com>
2025-09-24 18:44:00 -0500

    feat: :sparkles: Add Planes service Created planes_service.js using PlanesRepository with business logic to perform CRUD operations on plans. by@Andre07g

---
+++ services/planes_service.js
@@ -0,0 +1,30 @@
+import PlanesRepositorio from "../repositories/planes_repository.js";
+
+export default class PlanesService{
+    constructor(base,cliente){
+        this.repositorio=new PlanesRepositorio(base, cliente);
+    }
+
+    async crearPlan(planObj){
+        if(!planObj.nombre ||!planObj.duracion ||!planObj.metaFisica ||!planObj.nivel ||!planObj.precio){
+            throw new Error("El plan debe tener nombre, duracion, meta fisica, nivel y precio");
+        }
+        return await this.repositorio.crear(planObj)
+    }
+
+    async editarPlan(id,planObj){
+        if(!planObj.nombre ||!planObj.duracion ||!planObj.metaFisica ||!planObj.nivel || !planObj.precio){
+            throw new Error("El plan debe tener nombre, duracion, meta fisica y nivel");
+        }
+        return await this.repositorio.editar(id, planObj)
+    }
+
+    async listarPlanes(){
+        return await this.repositorio.listar();
+    }
+
+    async eliminarPlan(planObj){
+        return await this.repositorio.eliminar(planObj)
+    }
+    
+}
