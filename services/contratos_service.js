c0784e2b6a3688576c82fa74e3890b3f2c5a00ee
Andre07G <eleal087@gmail.com>
2025-09-24 18:41:51 -0500

    feat: :sparkles: Add Contratos service Created contratos_service.js integrating Clientes, Planes, and Contratos repositories. When creating a contract, it automatically assigns a plan to a client. by@Andre07g

---
+++ services/contratos_service.js
@@ -0,0 +1,61 @@
+import ContratosRepositorio from "../repositories/contratos_repository.js";
+import ClientesRepositorio from "../repositories/clientes_repository.js";
+import PlanesRepositorio from "../repositories/planes_repository.js";
+
+export default class ContratosService {
+    constructor(base, cliente) {
+        this.repositorio = new ContratosRepositorio(base, cliente);
+        this.clientesRepo = new ClientesRepositorio(base, cliente);
+        this.planesRepo = new PlanesRepositorio(base, cliente);
+    }
+
+    async crearContrato(contratoObj, clienteObj, planObj) {
+        if (!contratoObj || clienteObj || planObj) {
+            throw new Error("Datos incompletos");
+        }
+        const session = this.cliente.startSession();
+        try {
+            await session.withTransaction(async () => {
+                await this.clientesRepo.asignarPlan(planObj._id);
+                const contrato = {
+                    clienteNombre: clienteObj.nombre,
+                    clienteId: new Object(clienteObj._id),
+                    planNombre: planObj.nombre,
+                    planId: planObj._id,
+                    duracion: contratoObj.duracion,
+                    precio: planObj.precio,
+                    fechaInicio: new Date(),
+                    fechaFinalizacion: null
+                };
+                await this.repositorio.crear(contrato);
+                console.log("Plan adquirido correctamente, contrato creado de forma exitosa");
+            })
+        }
+        catch (error) {
+            console.error("Error al realizar contrato:", error.message);
+        } finally {
+            await session.endSession();
+        }
+
+    }
+
+    async listarContratos() {
+        return await this.repositorio.listarContratos();
+    }
+
+    async finalizar(contratoObj) {
+        const session = this.cliente.startSession();
+        try {
+            await session.withTransaction(async () => {
+                await this.repositorio.finalizar(contratoObj);
+                await this.repositorio.eliminarPlan(contratoObj.clienteId,contratoObj.planId);
+                console.log("Contrato cancelado exitosamente, el plan fue removido")
+            })
+        } catch (error) {
+            console.error("Error al realizar contrato:", error.message);
+        } finally {
+            await session.endSession();
+        }
+    }
+}
+
