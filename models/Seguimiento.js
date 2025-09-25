7b34c78cc09868cc9c146305a2da1f95835a652a
KarolainReyes <reyeskarol0412@gmail.com>
2025-09-25 07:21:11 -0500

    feat: :sparkles: seguimiento-model  add Seguimiento model to the models folder
     Create a new `Seguimiento` class inside the `models` directory to manage
     client progress tracking. This class will serve as the foundation for
     handling progress records and related operations in future CLI features.
    
    by@KarolainReyes

---
+++ models/Seguimiento.js
@@ -0,0 +1,10 @@
+export default class Seguimiento {
+
+    constructor(clienteId, pesoActual, porcentajeDeGrasa, medidas, comentarios){
+        this.clienteId = clienteId
+        this.pesoActual = pesoActual
+        this.porcentajeDeGrasa = porcentajeDeGrasa
+        this.medidas = medidas
+        this.comentarios = comentarios
+    }
+}
