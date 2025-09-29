8be075be2dbcc3e396eed12e54085ebb41d35973
KarolainReyes <reyeskarol0412@gmail.com>
2025-09-29 11:26:03 -0500

    fix: :bug: Correct destructuring logic in menuPrincipal.js Adjusted the destructuring of options in the main menu to ensure proper variable handling and avoid runtime errors. by@KarolainReyes

--- cli/menuPrincipal.js
+++ cli/menuPrincipal.js
@@ -18,7 +18,7 @@ export async function menuPrincipal(base, cliente) {
     NutricionCommand.setBase(base);
     console.log('Bienvenido');
     while (salirMain == false) {
-        const { opcion } = await opciones("Clientes", "Planes de Entrenamiento", "Seguimiento", "Contratos", "Gestión Financiera", "Salir")
+        const  opcion  = await opciones("Clientes", "Nutricion","Planes de Entrenamiento", "Seguimiento", "Contratos", "Gestión Financiera", "Salir")
         console.log(opcion)
         switch (opcion) {
             case "Clientes":
@@ -30,10 +30,14 @@ export async function menuPrincipal(base, cliente) {
             case "Seguimiento":
                 await subMenuSeguimiento();
                 break
+            case "Nutricion":
+                await subMenuNutricion();
+                break
             case "Contratos":
                 await subMenuContratos();
                 break
             case "Gestión Financiera":
+                await subMenuGestion();
                 break
             case "Salir":
                 salirMain = true;
@@ -52,7 +56,7 @@ export async function menuPrincipal(base, cliente) {
 async function subMenuClientes() {
     let exitCliente = false;
     while (!exitCliente) {
-        const { opcionCliente } = await opciones("Crear cliente", "Editar cliente", "Eliminar cliente", "Buscar cliente", "Listar clientes", "Regresar al menú anterior")
+        const opcionCliente  = await opciones("Crear cliente", "Editar cliente", "Eliminar cliente", "Buscar cliente", "Listar clientes", "Regresar al menú anterior")
 
         switch (opcionCliente) {
             case "Crear cliente":
@@ -85,7 +89,7 @@ async function subMenuPlanes() {
     let exitPlanes = false;
     while (!exitPlanes) {
 
-        const { opcionPlanes } = await opciones("Crear plan de entrenamiento", "Editar un plan de entrenamiento", "Eliminar un plan de entrenamiento", "Listar los planes de entrenamiento", "Regresar al menú anterior")
+        const  opcionPlanes  = await opciones("Crear plan de entrenamiento", "Editar un plan de entrenamiento", "Eliminar un plan de entrenamiento", "Listar los planes de entrenamiento", "Regresar al menú anterior")
 
         switch (opcionPlanes) {
             case "Crear plan de entrenamiento":
@@ -114,7 +118,7 @@ async function subMenuPlanes() {
 async function subMenuSeguimiento() {
     let exitSeguimiento = false;
     while (!exitSeguimiento) {
-        const { opcionSeguimiento } = await opciones("Registrar avance", "Eliminar Avance", "Ver avance de un cliente", "Regresar al menú anterior")
+        const opcionSeguimiento  = await opciones("Registrar avance", "Eliminar Avance", "Ver avance de un cliente", "Regresar al menú anterior")
 
         switch (opcionSeguimiento) {
             case "Registrar avance":
@@ -141,7 +145,7 @@ async function subMenuSeguimiento() {
 async function subMenuContratos() {
     let exitContratos = false;
     while (!exitContratos) {
-        const { opcionContratos } = await opciones("Crear contrato", "Finalizar contrato", "Ver contratos", "Regresar al menú anterior")
+        const  opcionContratos  = await opciones("Crear contrato", "Finalizar contrato", "Ver contratos", "Regresar al menú anterior")
 
         switch (opcionContratos) {
             case "Crear contrato":
@@ -164,10 +168,11 @@ async function subMenuContratos() {
 //                 GESTIÓN FINANCIERA
 //===================================================
 
-async function subMenuGestion() {
+async function subMenuGestion(cliente) {
     let exitGestion = false;
     while (!exitGestion) {
-        const { opcionGestion } = await opciones("Crear Movimiento", "Consultar Historial de Movimientos", "Balance por clientes", "Balance general", "Volver al menu anterior")
+        const  opcionGestion 
+         = await opciones("Crear Movimiento", "Consultar Historial de Movimientos", "Balance por clientes", "Balance general", "Volver al menu anterior")
 
         switch (opcionGestion) {
             case "Crear Movimiento":
@@ -189,3 +194,24 @@ async function subMenuGestion() {
     }
 };
 
+async function subMenuNutricion(){
+    let exitNutricion = false;
+    while (!exitNutricion) {
+        const  opcionNutricion
+         = await opciones("Crear Plan Nutricional","Eliminar Plan Nutricional","Listar Planes","Volver al menu anterior")
+        switch (opcionNutricion) {
+            case "Crear Plan Nutricional":
+                await NutricionCommand.CrearPlan();
+                break;
+            case "Eliminar Plan Nutricional":
+                await NutricionCommand.Eliminarplan();
+                break;
+            case "Listar Planes":
+                await NutricionCommand.ListarPlanes();
+                break;
+            case "Volver al menu anterior":
+                exitGestion = true;
+                break;
+        }
+    }
+}
