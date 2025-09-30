af2c9b4f0b702719b20e00c6bc764ea0bd53a9a0
Andre07G <eleal087@gmail.com>
2025-09-29 20:29:17 -0500

    feat: :lock: Add admin login for security Implemented an admin authentication mechanism to enhance application security and restrict access to sensitive operations. by@Andre07g

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { conectar, obtenerBase, obtenerCliente, cerrarConexion } from './config/db_conection.js';
-import {menuPrincipal} from "../cli/menuPrincipal.js"; 
+import {menuPrincipal} from "../cli/menuPrincipal.js";
+import {sleep,preguntar } from "../utils/utilidades.js" 
+import chalk from "chalk";
 const uri = process.env.MONGO_URI;
-
+const claveAdmin = process.env.CLAVE;
 try {
   await conectar(uri, "MetaFit");
   const base = obtenerBase();
   const cliente = obtenerCliente();
-  await menuPrincipal(base,cliente);
-
+  await sleep(1000);
+  console.clear()
+  let contraseña=await preguntar ("Ingrese la contraseña");
+  if(contraseña==claveAdmin){console.log("Contraseña correcta");await sleep(1000);await menuPrincipal(base,cliente);}
+  else{console.log(chalk.redBright("Contraseña incorrecta"));await sleep(1000);console.clear()}
 } catch (error) {
   console.error("Error en la aplicación:", error);
 } finally {
